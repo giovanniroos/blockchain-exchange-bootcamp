ed11e0c99e726f027aa34f1b37290a2e8cb2d5f5
giovanni1 <giovannir@discovery.co.za>
2021-05-27 19:16:28 +0200

    Align Token contract with ERC20

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,20 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
 contract Token {
-    string public name = "My Name";
+    using SafeMath for uint;
+
+    // Variables
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+        constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
+
+    
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -6,11 +6,36 @@ require('chai')
 
 
 contract('Token', (accounts) => {
-    describe('deployment', () =>{
-        it('tracs the name', async() =>{
-            const token = await Token.new();
-            const result = await token.name();
-            result.should.equal('My Name');
+    const name = 'DApp Token'
+    const symbol = 'DAPP'
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+    let token
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
+
+  describe('deployment', () => {
+        it('tracks the name', async () => {
+        const result = await token.name()
+        result.should.equal(name)
         })
+
+        it('tracks the symbol', async ()  => {
+        const result = await token.symbol()
+        result.should.equal(symbol)
+        })
+
+        it('tracks the decimals', async ()  => {
+        const result = await token.decimals()
+        result.toString().should.equal(decimals)
+        })
+
+        it('tracks the total supply', async ()  => {
+        const result = await token.totalSupply()
+        result.toString().should.equal(totalSupply)
+        })
+
     })
 })
